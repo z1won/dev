4ad97d94a73e1854307a18fe7d89df79e730ef04
상큼 김 <thatsvery@hotmail.co.kr>
2021-03-24 00:36:24 +0900

    초기 커밋.

---
+++ BootStrapDemo/Pages/About.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootStrapDemo.Pages
+{
+    // razor파일의 코드비하인드 파일은 ComponentBase를 기본적으로 상속받는다.
+    // 그러므로 :ComponentBase는 생략가능
+    public partial class About : ComponentBase
+    {
+        private readonly string Title = "정보(About) Page";
+        private string SubTitle = "사이트 정보";
+        protected override void OnInitialized()
+        {
+            SubTitle = DateTime.Now.ToLongTimeString();
+        }
+
+
+    }
+}
---
+++ BootStrapDemo/Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootStrapDemo.Pages
+{
+    public partial class Index
+    {
+
+
+
+
+        protected override void OnInitialized()
+        {
+            
+        }
+
+    }
+}
