6e4dafe6d3b50f762641a25537670c05113e831c
z1won <78031814+z1won@users.noreply.github.com>
2026-09-11 20:08:30 +0900

    feat: register GitHub API client

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using dev.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp =>
+{
+    var client = new HttpClient
+    {
+        BaseAddress = new Uri("https://api.github.com/")
+    };
+    client.DefaultRequestHeaders.UserAgent.ParseAdd("z1won-dev-portal");
+    client.DefaultRequestHeaders.Accept.ParseAdd("application/vnd.github+json");
+    return client;
+});
+builder.Services.AddScoped<GitHubApiClient>();
 
 await builder.Build().RunAsync();
